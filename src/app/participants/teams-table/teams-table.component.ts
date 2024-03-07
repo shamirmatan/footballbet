e8c311d97d962cf2909fa1752f74cfe42110ee39
Matan Shamir <matan.shamir@riskified.com>
2024-03-07 18:20:31 +0200

    sort teams

--- src/app/participants/teams-table/teams-table.component.ts
+++ src/app/participants/teams-table/teams-table.component.ts
@@ -1,11 +1,15 @@
-import {Component, Input} from "@angular/core";
+import {Component, Input, OnInit} from "@angular/core";
 
 @Component({
   selector: 'app-teams-table',
   templateUrl: './teams-table.component.html',
   styleUrls: ['./teams-table.component.css']
 })
-export class TeamsTableComponent {
+export class TeamsTableComponent implements OnInit {
   displayedColumns: string[] = ['logo','name', 'games', 'wins', 'draws', 'losses', 'qualifications', 'points'];
   @Input() teams: Team[];
+
+  ngOnInit() {
+    this.teams = this.teams.sort((a, b) => b.points - a.points);
+  }
 }
